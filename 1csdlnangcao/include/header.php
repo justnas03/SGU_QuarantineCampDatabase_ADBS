4f02fd086ac91a6c74f2f72964a98cc1a2337ebb
Thuan Do <93421674+justnas03@users.noreply.github.com>
2023-12-06 19:58:21 +0700

    Add files via upload

---
+++ 1csdlnangcao/include/header.php
@@ -0,0 +1,69 @@
+
+<header class="header">
+        <div class="info-admin">
+            <ul class="list-admin">
+                <li>
+                    <div class="box-1"></div>
+                    <i class='bx bx-heart logo-main'></i>
+                </li>
+                <li class="notification-and-admin">
+                    <div class="information-admin">
+                        <div class="sub-menu-user" id="sub_menu_user">
+                            <ul class="list-sub">
+                                <li class="sub-item">
+                                    <a href="#">
+                                        <i class='bx bx-log-out nav_icon'></i>
+                                        <span class="nav__name">Log out</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="name-user">
+                            <p>CV19 Camp</p>
+                            <span>Admin</span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </header>
+    <div class="l-navbar" id="navbar">
+        <nav class="nav">
+            <div>
+                <div class="nav__brand">
+                    <ion-icon name="menu-outline" class="nav__toggle" id="nav-toggle"></ion-icon>
+                    <a href="./Benhnhan.php" class="nav__logo">CV19 Camp</a>
+                </div>
+                <div class="nav__list">
+                    <a href="./Benhnhan.php" class="nav__link">
+                        <i class='bx bxs-first-aid nav_icon'></i>
+                        <span class="nav__name">Quản lý bệnh nhân</span>
+                    </a>
+                    <a href="./Phong.php" class="nav__link">
+                        <i class='bx bx-building nav_icon'></i>
+                        <span class="nav__name">Quản lý Phòng</span>
+                    </a>
+                    <a href="./xetnghiem.php" class="nav__link">
+                        <i class='bx bx-injection nav_icon'></i>
+                        <span class="nav__name">Quản lý Xét Nghiệm</span>
+                    </a>
+                    <a href="./Nhanvien.php" class="nav__link">
+                        <i class='bx bxs-user-account nav_icon'></i>
+                        <span class="nav__name">Quản lý Nhân Viên</span>
+                    </a>
+                    <a href="./dieutri.php" class="nav__link">
+                        <i class='bx bx-plus-medical nav_icon'></i>
+                        <span class="nav__name">Quản lý Điều Trị</span>
+                    </a>
+                    <a href="./thuoc.php" class="nav__link">
+                        <i class='bx bx-capsule nav_icon'></i>
+                        <span class="nav__name">Quản lý Thuốc</span>
+                    </a>
+                </div>
+            </div>
+        </nav>
+    </div>
+
+
+
+    
